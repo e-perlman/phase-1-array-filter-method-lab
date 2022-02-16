6a27d9e1259e84ae864e0cef22efdeed8857cefd
Emma Perlman <emmajperlman@gmail.com>
2022-02-15 20:54:18 -0800

    commit tree b02e79318c13190f6f64ea21bdeca2c07d678347

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(array,string){
+    return array.filter(element=>{
+        let upElement=element.toUpperCase()
+        let upString=string.toUpperCase()
+        return upElement===upString
+     }
+    )
+}
+
+function fuzzyMatch(array,string){
+    return array.filter(element=>{
+        if (element.indexOf(string)===0){
+            return element
+        }
+    })
+}
+
+function matchName(array,string){
+    return array.filter(element=>element.name===string)
+}
